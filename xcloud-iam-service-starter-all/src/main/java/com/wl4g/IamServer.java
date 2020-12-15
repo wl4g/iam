caefb3d1eacecdd7871ee4274f16e652708a50c6
wanglsir <wangl@shangmaikeji.com>
2020-12-15 11:26:34 +0800

    fix: apiversions management

--- xcloud-iam-service-starter-all/src/main/java/com/wl4g/IamServer.java
+++ xcloud-iam-service-starter-all/src/main/java/com/wl4g/IamServer.java
@@ -19,11 +19,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.wl4g.components.core.web.versions.annotation.EnableApiVersionMapping;
+import com.wl4g.components.core.web.versions.annotation.EnableApiVersionManagement;
 import com.wl4g.components.data.annotation.EnableComponentsData;
 import com.wl4g.iam.annotation.EnableIamServer;
 
-@EnableApiVersionMapping
+@EnableApiVersionManagement
 @EnableIamServer
 @MapperScan("com.wl4g.iam.data")
 @EnableComponentsData
