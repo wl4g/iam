a49e174f1211ea8bb3409b70094d66ba5b92f263
heweijie <babaa1f4@163.com>
2021-04-22 16:04:20 +0800

    fix: bug

--- xcloud-iam-service-data/src/main/java/com/wl4g/iam/data/RoleMenuDao.java
+++ xcloud-iam-service-data/src/main/java/com/wl4g/iam/data/RoleMenuDao.java
@@ -15,17 +15,17 @@
  */
 package com.wl4g.iam.data;
 
-import org.apache.ibatis.annotations.Param;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import static org.springframework.web.bind.annotation.RequestMethod.*;
-
 import com.wl4g.component.integration.feign.core.annotation.FeignConsumer;
 import com.wl4g.iam.common.bean.RoleMenu;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * {@link RoleMenuDao}
@@ -64,6 +64,6 @@ public interface RoleMenuDao {
 	int updateByPrimaryKey(@RequestBody RoleMenu record);
 
 	@RequestMapping(method = POST, value = "/insertBatch")
-	int insertBatch(@RequestParam("roleMenus") @Param("roleMenus") List<RoleMenu> roleMenus);
+	int insertBatch(@RequestBody @Param("roleMenus") List<RoleMenu> roleMenus);
 
 }
