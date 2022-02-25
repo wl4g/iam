9006bf2ad3acaae7af853a6c379c2306c6e9281d
wl4g <983708408@qq.com>
2022-02-25 18:31:03 +0800

    fix: switched feign istio

--- services/service-starter-facade/src/main/resources/META-INF/bootstrapping.groovy
+++ services/service-starter-facade/src/main/resources/META-INF/bootstrapping.groovy
@@ -52,7 +52,7 @@ class IamFacadeBootstrappingConfigurer implements IBootstrappingConfigurer {
 		if (isPresent("org.springframework.cloud.openfeign.FeignClient") && isPresent("org.springframework.cloud.openfeign.FeignAutoConfiguration")) {
 			configName.append(",iam-facade-scf");
 			location.append(",classpath:/scf/")
-		} else if (isPresent("com.wl4g.infra.rpc.feign.core.annotation.FeignConsumer")) {
+		} else if (isPresent("com.wl4g.infra.integration.feign.core.annotation.FeignConsumer")) {
 			configName.append(",iam-facade-sbf");
 			location.append(",classpath:/sbf/")
 		} else if (isPresent("com.alibaba.dubbo.rpc.Filter") && isPresent("com.alibaba.boot.dubbo.autoconfigure.DubboAutoConfiguration")) {
