a7b8e875e835b25d1836c2dc57fca3842ff5ee64
wl4g <wanglsir@gmail.com>
2022-06-10 15:21:38 +0800

    fix: add continue fetch
    fix: continue fetcher
    
    feat: add statistics

--- tools/update_geodata/china/fetch_geodata_from_aliyun.py
+++ tools/update_geodata/china/fetch_geodata_from_aliyun.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import subprocess
+import json
 import random
 import time
 import urllib.request
@@ -38,8 +40,10 @@ INIT_DELAY = 3  # Initial delay seconds.
 MAX_RETRIES = 3  # Max attempts with 403
 BATCH_TASKS = 50  # Add batch tasks count.
 
+
 os.environ['GEVENT_SUPPORT'] = 'True'
 monkey.patch_all()  # Required for time-consuming operations
+
 
 # current_dir = os.getcwd()
 entrypoint_dir, entrypoint_file = os.path.split(
@@ -64,11 +68,11 @@ def do_fetch(url, outputFile, adcode, name, level):
     return resp
 
 
-def do_retries_fetch(base_uri, adcode, name, level):
+def do_retries_fetch(adcode, name, level):
     suffix = adcode + "_full.json"
     if int(level) >= 2:
         suffix = adcode + ".json"
-    url = base_uri + suffix
+    url = GEO_BASE_URI + suffix
     outputFile = output_dir + suffix
     try:
         retries = 0
@@ -86,34 +90,89 @@ def do_retries_fetch(base_uri, adcode, name, level):
     except Exception as e:
         print("Failed to fetch for %s/%s. reason: %s" % (name, suffix, e))
         with open(outputFile + ".err", "w", encoding="utf-8") as geofile:
-            geofile.write(
-                "ERROR: Failed to fetch for %s, caused by: %s" % (url, e))
+            errjson = {
+                "adcode": adcode,
+                "name": name,
+                "level": level,
+                "errmsg": ("ERROR: Failed to fetch for %s, caused by: %s" % (url, e))
+            }
+            geofile.write(json.dumps(errjson))
             geofile.close
 
 
 def fetch_all():
-    with open(entrypoint_dir + "/area_cn.csv", "r", encoding="utf-8") as csvfile:
+    files = os.listdir(output_dir)
+    if len(files) <= 0:
+        print("Full new fetching ...")
+        with open(entrypoint_dir + "/area_cn.csv", "r", encoding="utf-8") as csvfile:
+            greenlets = []
+            batchs = 0
+            reader = csv.reader(csvfile)
+            headers = next(reader)
+            for row in reader:
+                adcode = row[0]
+                # parent = row[1]
+                name = row[2]
+                level = row[3]
+
+                # for testing.
+                # print("add task for '%s/%s/%s/%s'" % (adcode, parent, name, level))
+                # do_retries_fetch(adcode, name, level)
+
+                if int(level) <= FOR_LEVEL_LE:
+                    greenlets.append(gevent.spawn(
+                        do_retries_fetch, adcode, name, level))
+                if len(greenlets) % BATCH_TASKS == 0:
+                    batchs += 1
+                    print("[%d] Submit batch tasks ..." % (batchs))
+                    gevent.joinall(greenlets, timeout=30)
+    else:
+        print("Continue last uncompleted or failed fetch tasks from '%s'" %
+              (output_dir))
         greenlets = []
         batchs = 0
-        reader = csv.reader(csvfile)
-        headers = next(reader)
-        for row in reader:
-            adcode = row[0]
-            # parent = row[1]
-            name = row[2]
-            level = row[3]
+        for f in files:
+            if f.endswith(".err"):
+                errjsonStr = ""
+                errfileStr = output_dir + "/" + f
+                with open(errfileStr, "r", encoding="utf-8") as errfile:
+                    while True:
+                        line = errfile.readline(1024)
+                        if len(line) <= 0:
+                            break
+                        errjsonStr += line
 
-            # for testing.
-            # print("add task for '%s/%s/%s/%s'" % (adcode, parent, name, level))
-            # do_retries_fetch(GEO_BASE_URI, adcode, name, level)
+                errjson = json.loads(errjsonStr)
+                adcode = errjson['adcode']
+                name = errjson['name']
+                level = errjson['level']
 
-            if int(level) <= FOR_LEVEL_LE:
+                # for testing.
+                # print("add task for '%s/%s/%s/%s'" % (adcode, parent, name, level))
+                # do_retries_fetch(adcode, name, level)
+
+                # Continue add batch fetch tasks.
                 greenlets.append(gevent.spawn(
-                    do_retries_fetch, GEO_BASE_URI, adcode, name, level))
-            if len(greenlets) % BATCH_TASKS == 0:
-                batchs += 1
-                print("[%d] Submit batch tasks ..." % (batchs))
-                gevent.joinall(greenlets, timeout=30)
+                    do_retries_fetch, adcode, name, level))
+                if len(greenlets) % BATCH_TASKS == 0:
+                    batchs += 1
+                    print("[%d] Submit batch tasks ..." % (batchs))
+                    gevent.joinall(greenlets, timeout=30)
+                # Remove older err json file.
+                os.remove(errfileStr)
+
+
+def statistics():
+    success = 0
+    failure = 0
+    for f in os.listdir(output_dir):
+        if f.endswith(".err"):
+            failure += 1
+        else:
+            success += 1
+    print("-----------------------------------------------")
+    print("FETCHED Completed of SUCCESS: %d / FAILURE: %d" % (success, failure))
+    print("-----------------------------------------------")
 
 
 if __name__ == "__main__":
@@ -121,6 +180,7 @@ if __name__ == "__main__":
     try:
         os.nice(5)
         fetch_all()
+        statistics()
     except KeyboardInterrupt:
         print("Cancel fetch tasks ...")
         # gevent.killall()
